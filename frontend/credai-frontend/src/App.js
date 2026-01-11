86aba009218f1ed2fbdbe93fdecc4e8630ee6384
MeVaishnav <vaishnavcsbedradka@gmail.com>
2026-01-12 00:57:17 +0530

    updated the app.js file

--- frontend/credai-frontend/src/App.js
+++ frontend/credai-frontend/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Topbar from "./components/Topbar";
+import Dashboard from "./pages/Dashboard";
+import Vendors from "./pages/Vendors";
+import Analytics from "./pages/Analytics";
+import LoanApplications from "./pages/LoanApplications";
+import Settings from "./pages/Settings";
+import Help from "./pages/Help";
+import { Box } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Box sx={{ display: "flex" }}>
+        <Sidebar />
+        <Box sx={{ width: "100%" }}>
+          <Topbar />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/vendors" element={<Vendors />} />
+            <Route path="/analytics" element={<Analytics />} />
+            <Route path="/loan-apps" element={<LoanApplications />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/help" element={<Help />} />
+          </Routes>
+        </Box>
+      </Box>
+    </Router>
   );
 }
 
