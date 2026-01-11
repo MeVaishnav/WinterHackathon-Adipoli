73b8a7384f4f5cf0ed499b7c67ea18d48ca8482c
MeVaishnav <vaishnavcsbedradka@gmail.com>
2026-01-12 00:53:22 +0530

    created the LoanApplication page

---
+++ frontend/credai-frontend/src/pages/LoanApplication.js
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+const LoanApplications = () => {
+  return (
+    <Box p={3}>
+      <Typography variant="h5">Loan Applications Page</Typography>
+    </Box>
+  );
+};
+
+export default LoanApplications;
