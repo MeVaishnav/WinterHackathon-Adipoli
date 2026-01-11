2336d9e11e68eba245bf48634eaf41817a10a5bd
MeVaishnav <vaishnavcsbedradka@gmail.com>
2026-01-12 00:52:15 +0530

    created the analytics page

---
+++ frontend/credai-frontend/src/pages/Analytics.js
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+const Analytics = () => {
+  return (
+    <Box p={3}>
+      <Typography variant="h5">Analytics Page</Typography>
+    </Box>
+  );
+};
+
+export default Analytics;
